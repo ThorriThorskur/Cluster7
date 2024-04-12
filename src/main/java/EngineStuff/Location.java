ef11934ca205080037f4802afc335b5a664295de
lumeto_de_diamanto <gretar.vidir@gmail.com>
2024-04-12 10:40:43 +0000

    Added name field to location

--- src/main/java/EngineStuff/Location.java
+++ src/main/java/EngineStuff/Location.java
@@ -3,9 +3,11 @@ package EngineStuff;
 public class Location {
     float latitude;
     float longitude;
+    String name;
 
-    Location(float latitude, float longitude) {
+    Location(float latitude, float longitude, String name) {
         this.latitude = latitude;
         this.longitude = longitude;
+        this.name = name;
     }
 }

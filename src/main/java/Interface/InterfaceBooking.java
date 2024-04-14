48aec16c67a86ccb1b440fb66692b92c85d5114b
ThorriThorskur <73035171+ThorriThorskur@users.noreply.github.com>
2024-04-14 15:43:23 +0000

    Added InterfaceBooking and made a UI for flights

---
+++ src/main/java/Interface/InterfaceBooking.java
@@ -0,0 +1,30 @@
+package Interface;
+
+import java.util.Date;
+import java.util.UUID;
+
+public class InterfaceBooking {
+    public enum ServiceType {
+        Flight, Hotel, DayTrip
+    }
+
+    private UUID bookingId;
+    private ServiceType serviceType;
+    private long serviceId;
+    private Date bookingDate;
+
+    public InterfaceBooking(ServiceType serviceType, long serviceId, Date bookingDate) {
+        this.bookingId = UUID.randomUUID();
+        this.serviceType = serviceType;
+        this.serviceId = serviceId;
+        this.bookingDate = bookingDate;
+    }
+
+    public UUID getBookingId() { return bookingId; }
+    public ServiceType getServiceType() { return serviceType; }
+    public long getServiceId() { return serviceId; }
+    public Date getBookingDate() { return bookingDate; }
+
+    public void setBookingDate(Date bookingDate) { this.bookingDate = bookingDate; }
+}
+

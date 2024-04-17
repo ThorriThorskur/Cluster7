517ac876a1371392b6ed356c26e772d66f8435da
CatarinaLima-HI <123655795+CatarinaLima-HI@users.noreply.github.com>
2024-04-17 09:52:47 +0000

    the booking object only gets added to the database if the whole order is completed.

--- src/main/java/Controllers/BookFlightController.java
+++ src/main/java/Controllers/BookFlightController.java
@@ -100,8 +100,8 @@ public class BookFlightController {
         if (validateInput(name, passportNumber, address, email, phoneNumber)) {
             Passenger passenger = new Passenger(name, passportNumber, address, email, phoneNumber);
             BookingFlight booking = new BookingFlight(selectedFlight, seat, passenger, false, bags);
-            selectedFlight.getSeat(seat.getSeatName()).setBooked(true);
-            bookingFlightDB.insert(booking);
+            //selectedFlight.getSeat(seat.getSeatName()).setIsonHold(true);
+            //bookingFlightDB.insert(booking);
 
             Cart.getInstance().addBooking(booking);
 
